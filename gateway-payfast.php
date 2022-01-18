6fa172bf6576e492d5425b4ee4011cd657f06417
Brian Yu <brian@brianyu.me>
2022-01-18 15:40:49 -0500

    Update WP, WC tested up to

--- gateway-payfast.php
+++ gateway-payfast.php
@@ -7,8 +7,8 @@
  * Author URI: http://woocommerce.com/
  * Version: 1.4.20
  * Requires at least: 4.4
- * Tested up to: 5.8.3
- * WC tested up to: 6.1.0
+ * Tested up to: 5.8
+ * WC tested up to: 6.1
  * WC requires at least: 2.6
  *
  */
