650086e1cd075b317a8a76b54bd80f7fa29be27f
Dharmesh Patel <dspatel44@gmail.com>
2022-02-01 15:35:29 +0530

    enhancement/62: Added filter on payment data to send.

--- includes/class-wc-gateway-payfast.php
+++ includes/class-wc-gateway-payfast.php
@@ -296,6 +296,9 @@ class WC_Gateway_PayFast extends WC_Payment_Gateway {
 			$this->data_to_send['subscription_type'] = '2';
 		}
 
+		// Allow other plugins to extend this.
+		$this->data_to_send = apply_filters( 'woocommerce_gateway_payfast_payment_data_to_send', $this->data_to_send, $order_id );
+
 		$payfast_args_array = array();
 		$sign_strings = [];
 		foreach ( $this->data_to_send as $key => $value ) {
