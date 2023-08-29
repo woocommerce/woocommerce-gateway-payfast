3e6c1e387ac56775404fe7dcd43d698b0b9e9332
Ravinder Kumar <ravinder.kumar.apj@gmail.com>
2023-08-29 19:54:22 +0530

    tests: resolve loopback request issue

--- tests/e2e/wordpress-plugins/payfast-webhook-faker/payfast-webhook-faker.php
+++ tests/e2e/wordpress-plugins/payfast-webhook-faker/payfast-webhook-faker.php
@@ -19,9 +19,15 @@ add_filter( 'pre_http_request', function ( $result, $args, $url ) {
 
 // Fake the PayFast webhook.
 add_action( 'woocommerce_thankyou_payfast', function ( $order_id ) {
-	/* @var WC_Gateway_PayFast $paymentGateway */
-	$paymentGateway = WC()->payment_gateways()->payment_gateways()['payfast'];
-	$paymentGateway->handle_itn_request( ( new WebhookDataProvider( $order_id ) )->getData() );
+	wp_remote_post(
+		esc_url_raw(home_url('/?wc-api=wc_gateway_payfast')),
+		[
+			'headers' => [ 'Content-Type' => 'application/x-www-form-urlencoded' ],
+			'body'    => ( new WebhookDataProvider( $order_id ) )->getData(),
+			'format'  => 'body',
+			'sslverify' => false,
+		]
+	);
 }, 999 );
 
 
