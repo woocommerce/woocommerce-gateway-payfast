707f48a0683221f6265e3c3e4446fc53054d0405
faisal-alvi <faisalalvi33@gmail.com>
2023-08-29 19:03:11 +0530

    Bump WordPress Tested up to version to 6.3

--- gateway-payfast.php
+++ gateway-payfast.php
@@ -7,7 +7,7 @@
  * Author URI: http://woocommerce.com/
  * Version: 1.5.7
  * Requires at least: 6.1
- * Tested up to: 6.2
+ * Tested up to: 6.3
  * WC tested up to: 7.8
  * WC requires at least: 7.2
  * Requires PHP: 7.2
