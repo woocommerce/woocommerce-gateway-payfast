325db1e51c7e56d1723f74ef99e9375dea3ed1d6
Dharmesh Patel <dspatel44@gmail.com>
2025-09-11 11:03:10 +0530

    E2E: Tweak Update payfast settings.

--- tests/e2e/utils/index.js
+++ tests/e2e/utils/index.js
@@ -75,16 +75,19 @@ export async function editPayfastSetting( {page, settings} ) {
 
 			case 'merchant_id':
 				const merchantIdSettingLocator = await page.getByLabel( 'Merchant ID', {exact: true} );
+				await merchantIdSettingLocator.clear();
 				await merchantIdSettingLocator.fill( settings.merchant_id );
 				break;
 
 			case 'merchant_key':
 				const merchantKeySettingLocator = await page.getByLabel( 'Merchant Key', {exact: true} );
+				await merchantKeySettingLocator.clear();
 				await merchantKeySettingLocator.fill( settings.merchant_key );
 				break;
 
 			case 'passphrase':
 				const passphraseSettingLocator = await page.getByLabel( 'Passphrase', {exact: true} );
+				await passphraseSettingLocator.clear();
 				await passphraseSettingLocator.fill( settings.passphrase );
 				break;
 
