553ccc1945a30972248f80b5831dfe32fd081e9d
Sumit Bagthariya <67687255+qasumitbagthariya@users.noreply.github.com>
2025-11-27 22:25:02 +0530

    Bump WooCommerce "tested up to" version 10.4

--- woocommerce-gateway-payfast.php
+++ woocommerce-gateway-payfast.php
@@ -9,8 +9,8 @@
  * Version: 1.7.4
  * Requires at least: 6.7
  * Tested up to: 6.8
- * WC requires at least: 10.1
- * WC tested up to: 10.3
+ * WC requires at least: 10.2
+ * WC tested up to: 10.4
  * Requires PHP: 7.4
  * PHP tested up to: 8.4
  *
